8b9a4bc964c1670a124b546fb2e4042e1497d285
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-02 12:50:36 -0800

    write GetAll test and receive fail(method not defined).

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -12,7 +12,7 @@ namespace ToDoList.Tests
     {
       Category.ClearAll();
     }
-    
+
     [TestMethod]
     public void CategoryConstructor_CreatesInstanceOfCategory_Category()
     {
@@ -36,6 +36,18 @@ namespace ToDoList.Tests
       Category newCategory = new Category(name);
       int result = newCategory.Id;
       Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      List<Category> newList = new List<Category> { newCategory1, newCategory2 };
+      List<Category> result = Category.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
