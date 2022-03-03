8d5b6db15fbf9297d1a37a48105308b66efce969
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-02 16:04:15 -0800

    move Item Create route to bottom of CategoriesController.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -21,19 +21,6 @@ namespace ToDoList.Controllers
       return View();
     }
 
-    [HttpPost("/categories/{categoryId}/items")]
-    public ActionResult Create(int categoryId, string itemDescription)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Category foundCategory = Category.Find(categoryId);
-      Item newItem = new Item(itemDescription);
-      foundCategory.AddItem(newItem);
-      List<Item> categoryItems = foundCategory.Items;
-      model.Add("items", categoryItems);
-      model.Add("category", foundCategory);
-      return View("Show", model);
-    }
-
     [HttpPost("/categories")]
     public ActionResult Create(string categoryName)
     {
@@ -50,6 +37,19 @@ namespace ToDoList.Controllers
       model.Add("category", selectedCategory);
       model.Add("items", categoryItems);
       return View(model);
+    }
+
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category foundCategory = Category.Find(categoryId);
+      Item newItem = new Item(itemDescription);
+      foundCategory.AddItem(newItem);
+      List<Item> categoryItems = foundCategory.Items;
+      model.Add("items", categoryItems);
+      model.Add("category", foundCategory);
+      return View("Show", model);
     }
   }
 }
