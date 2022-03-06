775867f802f1d986d3f287c71963a65fafe03aa7
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-06 15:37:20 -0800

    add code and pass Item Save test.

--- ToDoList/Models/Items.cs
+++ ToDoList/Models/Items.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    public int Id { get; }
+    public int Id { get; set; }
     public Item(string description)
     {
       Description = description;
@@ -25,8 +25,9 @@ namespace ToDoList.Models
       else
       {
         Item newItem = (Item) otherItem;
+        bool idEquality = (this.Id == newItem.Id);
         bool descriptionEquality = (this.Description == newItem.Description);
-        return descriptionEquality;
+        return (idEquality && descriptionEquality);
       }
     }
     public static List<Item> GetAll()
@@ -72,7 +73,21 @@ namespace ToDoList.Models
 
     public void Save()
     {
-      
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = "INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter param = new MySqlParameter();
+      param.ParameterName = "@ItemDescription";
+      param.Value = this.Description;
+      cmd.Parameters.Add(param);    
+      cmd.ExecuteNonQuery();
+      Id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
   }
 }
