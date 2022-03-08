ee9d1b65cf450f6d968acf7f4f8e4976935ec135
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-08 15:05:34 -0800

    build out Categories controller.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,8 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
