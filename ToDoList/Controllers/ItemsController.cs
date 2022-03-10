82a2d0cd40e0dea154b6dfa2b4cc46fc2e8213a1
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-10 13:45:48 -0800

    update Delete functionality. Through page 8 of Many-to-Many relationships.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -70,41 +70,50 @@ namespace ToDoList.Controllers
       _db.Entry(item).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
-      }
+    }
 
-      public ActionResult AddCategory(int id)
+    public ActionResult AddCategory(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      ViewBag.PageTitle = "Add Category to Item";
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult AddCategory(Item item, int CategoryId)
+    {
+      if (CategoryId != 0)
       {
-        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-        ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-        ViewBag.PageTitle = "Add Category to Item";
-        return View(thisItem);
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+        _db.SaveChanges();
       }
+      return RedirectToAction("Index");
+    }
 
-      [HttpPost]
-      public ActionResult AddCategory(Item item, int CategoryId)
-      {
-        if (CategoryId != 0)
-        {
-          _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
-          _db.SaveChanges();
-        }
-        return RedirectToAction("Index");
-      }
+    [HttpPost]
+    public ActionResult DeleteCategory(int joinId)
+    {
+      var joinEntry = _db.CategoryItem.FirstOrDefault(entry => entry.CategoryItemId == joinId);
+      _db.CategoryItem.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult Delete(int id)
-//     {
-//       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//       ViewBag.PageTitle = "Delete Item";
-//       return View(thisItem);
-//     }
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.PageTitle = "Delete Item";
+      return View(thisItem);
+    }
 
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//       _db.Items.Remove(thisItem);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
